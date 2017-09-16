2939008c221d76b8a3db4e9e62b6fa76cbc0ce50
vagrant <vagrant@homestead>
2017-09-16 05:32:00 +0000

    replace input with textarea in contact page

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -21,7 +21,8 @@
       <div class="form-group">
         <label class="control-label col-sm-2" for="text">Message:</label>
         <div class="col-sm-10"> 
-          <input type="textarea" class="form-control" id="text" placeholder="Write your message here!">
+          <textarea class="form-control" id="text" placeholder="Write your message here!">
+          </textarea>
         </div>
       </div>
       <div class="form-group"> 
